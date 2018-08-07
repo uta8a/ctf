d85314c75b3cc57126f3ab971d0e7d226640f386
kaito_tateyama <noumodulity@gmail.com>
2018-08-07 16:01:18 +0900

    [add] cpaw completegit add -Agit add -A

---
+++ cpawctf/level_3/4/new_4.py
@@ -0,0 +1,10 @@
+# extgcd
+def xgcd(b, a):
+    x0, x1, y0, y1 = 1, 0, 0, 1
+    while a != 0:
+        q, b, a = b // a, a, b % a
+        x0, x1 = x1, x0 - q * x1
+        y0, y1 = y1, y0 - q * y1
+    return  b, x0, y0
+x = xgcd(11,13)
+print(x)
--- cpawctf/level_3/level_3.md
+++ cpawctf/level_3/level_3.md
@@ -1,4 +1,7 @@
 # level_3
 | Problem number | knowledge |
 |:--------------:|:---------:|
-|1 |  |
+|1 | radare2, エスパー |
+|2 | SQLインジェクション |
+|3 | CRT |
+|4 | RSA, common modulus attack |
